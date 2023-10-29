85ed7887db4b76b7c28a51db9813ae13e559ca0c
RiddheshRajwadkar <riddheshrajwadkar49@gmail.com>
2023-10-29 11:21:08 +0530

    classes and datatypes done

---
+++ Day2/Classes_Identifier_Datatypes.java
@@ -0,0 +1,26 @@
+package Day2;
+
+public class Classes_Identifier_Datatypes {
+    
+    //^^Classes are a blueprint of an aobject
+
+    //Identifier are the names we give to a class, object, method, variable.
+    
+    //Datatpe is the type of data we store or use. It is divided in two types. First primitive datatypes and second non-primitive datatypes.
+    //Here Primitive datatypes are boolean, byte, char, short, int, long, float, double.
+    //And Non-primitive datatypes are String, Arrays etc.
+    public static void main(String[] args) {
+
+    int num1=10;
+    float num2=10;
+    long num3=10;
+    double num4=10;
+
+    System.out.println(num1);//10
+    System.out.println(num2);//10.0
+    System.out.println(num3);//10
+    System.out.println(num4);//10.0
+    
+    }
+     
+}
