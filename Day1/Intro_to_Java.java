27e35c38b5d9f8fc8bfe4c8da2c06bfbd74902ad
RiddheshRajwadkar <riddheshrajwadkar49@gmail.com>
2023-10-29 10:35:40 +0530

    Day1 complete

---
+++ Day1/Intro_to_Java.java
@@ -0,0 +1,15 @@
+/*
+ * What is Java?
+ * Java is a object oriented language and platform. It is a high level, robust and secure programming language.
+ * 
+ * What are the features of Java
+ * Simple
+ * Secure
+ * Object oriented
+ * robust
+ * platform independent
+ * portable
+ * multithreaded
+ * 
+ * 
+ */
