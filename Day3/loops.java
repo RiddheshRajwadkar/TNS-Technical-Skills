0b6a4f3807abd8a4dba134d1b8a3112ba7d51269
RiddheshRajwadkar <riddheshrajwadkar49@gmail.com>
2023-10-29 12:53:08 +0530

    loops done

--- Day3/loops.java
+++ Day3/loops.java
@@ -2,6 +2,10 @@ package Day3;
 
 public class loops {
     public static void main(String[] args) {
-        
+        //By using for loop the the code will be run 5 times and print the values from the array
+        int array[] = {1,2,3,4,5};
+        for(int i=0; i<array.length; i++){
+            System.out.print(array[i]);//12345
+        }
     }
 }
