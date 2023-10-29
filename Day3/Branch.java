76b81a8ee18e4e76f7a976c56fcca297a907556a
RiddheshRajwadkar <riddheshrajwadkar49@gmail.com>
2023-10-29 12:02:10 +0530

    Branching statements done

---
+++ Day3/Branch.java
@@ -0,0 +1,16 @@
+package Day3;
+
+public class Branch {
+    public static void main(String[] args) {
+        /*
+         * Branch statement are used to control the flow of the program/code
+         */
+        
+        //break: It is used to exit outside the loop/ control statements
+        
+        //continue: It is used to skip the current iteration to the start of the loop/ control statements
+
+        //returnL It is used to give a statement or value
+
+    }
+}
