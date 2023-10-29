01ce674eef937a13f0a38bc7ac2e150efcd25990
RiddheshRajwadkar <riddheshrajwadkar49@gmail.com>
2023-10-29 12:44:53 +0530

    loop done

---
+++ Day3/loop.java
@@ -0,0 +1,14 @@
+package Day3;
+
+public class loop {
+    public static void main(String[] args) {
+        int age=15;
+
+        if(age > 18){
+            System.out.println("You are eligible");
+        }
+        else{
+            System.out.println("You are not eligible");
+        }
+    }
+}
